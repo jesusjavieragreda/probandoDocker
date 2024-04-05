eb6379b565229f18b03a931b86987ec7f6b1a087
Jesus Javier <jesusjavieragreda@gmail.com>
2024-04-05 18:11:07 +0200

    Primer commit

---
+++ src/main/java/com/jesusjavier/pruebaDocker/PruebaDockerApplication.java
@@ -0,0 +1,13 @@
+package com.jesusjavier.pruebaDocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaDockerApplication.class, args);
+	}
+
+}
